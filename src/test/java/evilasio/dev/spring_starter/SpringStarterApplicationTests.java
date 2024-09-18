24655f88faddbbf672ac61f93e5f79877171857d
Evilasio Neto <evilasio_neto12@hotmail.com>
2024-09-17 21:33:22 -0300

    projeto iniciado

---
+++ src/test/java/evilasio/dev/spring_starter/SpringStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package evilasio.dev.spring_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
